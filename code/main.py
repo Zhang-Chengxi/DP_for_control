2c798048d0eccff9d8b1040f8b90c19a379eee0b
NUSSTF\rmizcxi <chengxi.morelia@gmail.com>
2018-09-18 18:36:02 +0800

    initial commit

---
+++ code/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 23 10:20 AM 2018
+
+@author: ZHANG Chengxi (rmizcxi@nus.edu.sg)
+"""
+
+import numpy as np
+import tensorflow as tf
+
+print(20000*1.04**4)
